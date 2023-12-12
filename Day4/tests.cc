e7bbd6f555657aa01fbe9adbb7775f09c3deb741
Chris Whitworth <chris.whitworth@microsoft.com>
2023-12-12 16:43:13 +0000

    Ready for Day 4

---
+++ Day4/tests.cc
@@ -0,0 +1,7 @@
+#include <catch2/catch_all.hpp>
+#include "day4.h"
+
+TEST_CASE( "Day 4")
+{
+    REQUIRE( true == true );
+}
--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # AOC23
+
+I'm doing it in C++ because I need the practise and haven't kept up to date with C++20 and beyond.
