f1999208f57e825494a9d4f03815657f99ab608c
Chris Whitworth <chris.whitworth@microsoft.com>
2023-12-14 14:29:30 +0000

    Day 5 part 2 and I can't be bothered to do it properly

--- Day5/main.cc
+++ Day5/main.cc
@@ -15,10 +15,21 @@ int main(int argc, char* argv[])
 
     Almanac a(lines);
     long long lowest = -1;
-    for(auto s: a.seeds) {
-        long long result = a.map_through(s);
-        if (result < lowest || lowest == -1) {
-            lowest = result;
+    // for(auto s: a.seeds) {
+    //     long long result = a.map_through(s);
+    //     if (result < lowest || lowest == -1) {
+    //         lowest = result;
+    //     }
+    // }
+
+    for(long long pair = 0; pair < a.seeds.size(); pair += 2)
+    {
+        for(long long seed = a.seeds[pair]; seed < a.seeds[pair] + a.seeds[pair+1]; seed++)
+        {
+            long long result = a.map_through(seed);
+            if (result < lowest || lowest == -1) {
+                lowest = result;
+            }
         }
     }
 
